d8633095a3fbb8cbeab4f9c66bc611666eeb2790
Ethan Garza <159205971+EthaGarz@users.noreply.github.com>
2024-06-19 18:08:17 -0500

    Create visited.js

---
+++ backend/visited.js
@@ -0,0 +1,7 @@
+let p = document.querySelector('p')
+
+let url = "https://gkpp2c75v8.execute-api.us-east-1.amazonaws.com/prod/items"
+
+fetch(url)
+.then(response => response.json())
+.then(json => p.innerHTML = json[0].get_count)
