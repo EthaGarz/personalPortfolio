80374fb9957246575e6cacdf6d9f020c3d2a75b4
Ethan Garza <159205971+EthaGarz@users.noreply.github.com>
2024-07-03 18:38:40 -0500

    Update visited.js

--- backend/visited.js
+++ backend/visited.js
@@ -1,6 +1,6 @@
 let p = document.querySelector('p')
 
-let url = 'https://gkpp2c75v8.execute-api.us-east-1.amazonaws.com/prod/items'
+let url = 'https://gkpp2c75v8.execute-api.us-east-1.amazonaws.com/prod/item'
 
 fetch(url)
 .then(response => response.json())
